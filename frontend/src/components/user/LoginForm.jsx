62aa240be4e8ecb8a28496238e213ab724fbbe30
Helen Yohanna Ventura Della Roveri <86626344+HelenDellaRoveri@users.noreply.github.com>
2025-11-03 07:31:09 -0300

    Add files via upload

--- frontend/src/components/user/LoginForm.jsx
+++ frontend/src/components/user/LoginForm.jsx
@@ -9,5 +9,39 @@ export default function LoginForm(){
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
 
-    
+    const login = async () => {
+        try{
+            const resposta = await axios.post('http://localhost:3001/auth/login',{
+                email,
+                password
+            });
+            localStorage.setItem('token', resposta.data.token);
+            alert(resposta.data.msg);
+            navigate('/');
+        }catch(err){
+            alert(err.response?.data?.msg || 'Erro ao logar');
+        }
+    };
+
+    return(
+        <Main icon="sign-in" title="Login" subtitle="Acesse sua Conta para continuar">
+            <div className="form">
+                <input 
+                type="text" 
+                className="form-control mb-12" 
+                placeholder="Email" 
+                value={email} 
+                onChange={ e => setEmail(e.target.value)}/>
+
+                <input 
+                type="password" 
+                className="form-control mb-12" 
+                placeholder="Senha" 
+                value={password} 
+                onChange={e => setPassword(e.target.value)}/>
+                
+                <button className="btn btn-primary" onClick={login}>Entrar</button>
+            </div>
+        </Main>
+    );
 }
